666e635a54c78f1d880ef8c434e02c8fc89bcbf6
Dan Yamins <dyamins@gmail.com>
2012-10-16 22:07:50 -0400

    more tests of pearsonr

--- yamutils/tests/tests_stats.py
+++ yamutils/tests/tests_stats.py
@@ -4,10 +4,10 @@ import yamutils.stats as ystats
 import scipy.stats as sstats
 import numpy as np
 
-def tests_pearsonr(rseed=0):
+def test_pearsonr(rseed=0):
     rng = np.random.RandomState(seed=rseed)
-    X = rng.uniform(size=(100, 50))
-    Y = rng.uniform(size=(10, 50))
+    X = rng.uniform(size=(200, 100))
+    Y = rng.uniform(size=(500, 100))
     
     yP = ystats.pearsonr(X.T, Y.T)[0]
     sP = np.array([[sstats.pearsonr(x, y)[0] for y in Y] for x in X])
@@ -15,9 +15,33 @@ def tests_pearsonr(rseed=0):
     print yP.shape
     print sP.shape
     diff = np.abs(yP - sP)
-    print diff
-    print diff.min(), diff.max()
 
     assert diff.sum() < 1e-5
 
 
+def test_pearsonr2(rseed=0):
+    rng = np.random.RandomState(seed=rseed)
+    X = 10 * rng.uniform(size=(200, 100))
+    Y = rng.uniform(size=(500, 100)) - 0.5
+    
+    yP = ystats.pearsonr(X.T, Y.T)[0]
+    sP = np.array([[sstats.pearsonr(x, y)[0] for y in Y] for x in X])
+
+    print yP.shape
+    print sP.shape
+    diff = np.abs(yP - sP)
+    assert diff.sum() < 1e-5
+    
+    
+def test_pearsonr3(rseed=0):
+    rng = np.random.RandomState(seed=rseed)
+    X = 10 * np.ones((200, 100)) + .001 * rng.uniform(size=(200, 100))
+    Y = .002 * rng.uniform(size=(500, 100))
+    
+    yP = ystats.pearsonr(X.T, Y.T)[0]
+    sP = np.array([[sstats.pearsonr(x, y)[0] for y in Y] for x in X])
+
+    print yP.shape
+    print sP.shape
+    diff = np.abs(yP - sP)
+    assert diff.sum() < 1e-5
