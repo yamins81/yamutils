e62da47a63d451d829ceeaecf9bdead6e2d01862
Dan Yamins <dyamins@gmail.com>
2013-09-17 10:49:36 -0400

    SONify in mongo utils

--- yamutils/mongo.py
+++ yamutils/mongo.py
@@ -1,4 +1,9 @@
 import copy
+import datetime
+import numpy as np
+
+from bson.objectid import ObjectId
+
 
 def combine_queries(a,b):
     a = copy.deepcopy(a)
@@ -25,3 +30,36 @@ def get_most_recent(coll, q, skip=0, limit=0, kwargs=None):
             F.append(c)
     return F
 
+
+def SONify(arg, memo=None):
+    if memo is None:
+        memo = {}
+    if id(arg) in memo:
+        rval = memo[id(arg)]
+    if isinstance(arg, ObjectId):
+        rval = arg
+    elif isinstance(arg, datetime.datetime):
+        rval = arg
+    elif isinstance(arg, np.floating):
+        rval = float(arg)
+    elif isinstance(arg, np.integer):
+        rval = int(arg)
+    elif isinstance(arg, (list, tuple)):
+        rval = type(arg)([SONify(ai, memo) for ai in arg])
+    elif isinstance(arg, dict):
+        rval = dict([(SONify(k, memo), SONify(v, memo))
+            for k, v in arg.items()])
+    elif isinstance(arg, (basestring, float, int, type(None))):
+        rval = arg
+    elif isinstance(arg, np.ndarray):
+        if arg.ndim == 0:
+            rval = SONify(arg.sum())
+        else:
+            rval = map(SONify, arg) # N.B. memo None
+    # -- put this after ndarray because ndarray not hashable
+    elif arg in (True, False):
+        rval = int(arg)
+    else:
+        raise TypeError('SONify', arg)
+    memo[id(rval)] = rval
+    return rval
