9391c7bbd1f8f4d6373499ec8f49f672db868478
Dan Yamins <dyamins@gmail.com>
2013-09-20 14:47:12 -0400

    removed mistaken commit of a merge conflict... sorry!

--- yamutils/aws.py
+++ yamutils/aws.py
@@ -25,15 +25,9 @@ def download_s3_bucket(bucket_name, target, credentials=None):
             if not os.path.exists(pathname):
                 print(n)
                 l.get_contents_to_filename(pathname)
-<<<<<<< HEAD
 
 
-def upload_s3_bucket(upload_dir, bucket_name, credentials=None):
-=======
-                
-    
-def upload_s3_bucket(upload_dir, bucket_name, prefix=None):
->>>>>>> ea1cf46ff85dae5ff0bd950f68da6c3020e1e57a
+def upload_s3_bucket(upload_dir, bucket_name, prefix=None, credentials=None):
     L = recursive_file_list(upload_dir)
     if credentials:
         conn = boto.connect_s3(*credentials)
@@ -41,19 +35,11 @@ def upload_s3_bucket(upload_dir, bucket_name, prefix=None):
         conn = boto.connect_s3()
     b = conn.get_bucket(bucket_name)
     for l in L:
-<<<<<<< HEAD
-        print(l)
-        k = b.new_key(l)
-        ft = os.path.splitext(l)[-1].strip('.').lower()
-        k.set_contents_from_filename(l, headers={'Content-Type' : 'image/%s' % ft})
-
-=======
         if prefix is not None:
             ln = prefix + '/' + l
         else:
             ln = l
         k = b.new_key(ln)
         print(l)
-        ft = os.path.splitext(l)[-1].strip('.')
+        ft = os.path.splitext(l)[-1].strip('.').lower()
         k.set_contents_from_filename(l, headers={'Content-Type' : 'image/%s' % ft})
->>>>>>> ea1cf46ff85dae5ff0bd950f68da6c3020e1e57a
