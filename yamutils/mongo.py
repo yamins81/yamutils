ec796122437cc878457631699b3fcd259d362334
Dan Yamins <dyamins@gmail.com>
2014-03-18 15:56:39 -0400

    bugfix

--- yamutils/mongo.py
+++ yamutils/mongo.py
@@ -47,7 +47,7 @@ def SONify(arg, memo=None):
         rval = int(arg)
     elif isinstance(arg, (list, tuple)):
         rval = type(arg)([SONify(ai, memo) for ai in arg])
-    elif isinstance(arg, collections.OrderedDict)
+    elif isinstance(arg, collections.OrderedDict):
         rval = collections.OrderedDict([(SONify(k, memo), SONify(v, memo))
             for k, v in arg.items()])
     elif isinstance(arg, dict):
